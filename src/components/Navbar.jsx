6406671a42989552ad8d474432f7841fda0fff3d
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-26 21:05:01 +0500

    Navbar add .

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <a href="">ixva Blog</a>
+        <div>
+            <a href="">Résumé</a>
+            <a href="">About me</a>
+            <a href="">Blog</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
